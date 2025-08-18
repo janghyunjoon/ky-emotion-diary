50ab80d97786b6de8c8daf6a3c337d01c717abe7
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-08-18 17:23:44 +0900

    .

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+
+import './App.css'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Diary from './pages/Diary'
+import Edit from './pages/Edit'
+import Home from './pages/Home'
+import Notfound from './pages/Notfound'
+import New from './pages/New'
+import { getEmotionImage } from './util/getEmotionImage'
+import Header from './components/Header'
+import Button from './components/Button';
+import { useReducer } from 'react'
+import React, { useRef } from 'react';
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용"
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용"
+  },
+]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state]
+    case "UPDATE":
+      return state.map((item)=>
+      String(item.id) === String((action.data.id) ?
+        action.data : item
+      ))
+    default:
+      return state
+  }
+}
+
+function App() {
+
+  const [data, dispatch] = useReducer(reducer, mockData)
+  const idRef = useRef(3)
+
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content
+      }
+    })
+  }
+
+  return (
+    <div>
+      <Header
+        leftChild={<Button text="left" />}
+        title="header title"
+        rightChild={<Button text="right" />}
+      />
+
+      <button onClick={() => onCreate(new Date().getTime(), 1, "hello")}>
+        일기 추가하기
+      </button>
+       <button onClick={() => onUpdate(new Date().getTime(), 3, "수정된 내용")}>
+        일기 수정하기
+      </button>
+
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/edit/:id' element={<Edit />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
